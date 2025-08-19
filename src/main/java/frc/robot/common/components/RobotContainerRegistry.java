d81d54635f6fa138e801d79070d3f449a5def824
strubium <113206902+strubium@users.noreply.github.com>
2025-08-19 17:30:56 -0500

    i will blow up alan in 90 seconds

--- src/main/java/frc/robot/common/components/RobotContainerRegistry.java
+++ src/main/java/frc/robot/common/components/RobotContainerRegistry.java
@@ -52,26 +52,43 @@ public class RobotContainerRegistry {
     private static List<Class<?>> getAllClasses() {
         List<Class<?>> classes = new ArrayList<>();
         String path = PACKAGE_NAME.replace('.', '/');
-        
-        try (InputStream is = RobotContainerRegistry.class.getClassLoader()
-                .getResourceAsStream(path);
-            JarInputStream jarStream = new JarInputStream(is)) {
-            
-            JarEntry entry;
-            while ((entry = jarStream.getNextJarEntry()) != null) {
-                if (entry.getName().endsWith(".class") && entry.getName().startsWith(path)) {
-                    String className = entry.getName()
-                            .replace('/', '.')
-                            .replace(".class", "");
-                    Class<?> clazz = Class.forName(className);
-                    classes.add(clazz);
+    
+        try {
+            var resources = RobotContainerRegistry.class.getClassLoader().getResources(path);
+            while (resources.hasMoreElements()) {
+                var url = resources.nextElement();
+                var protocol = url.getProtocol();
+    
+                if ("file".equals(protocol)) {
+                    // Running from exploded class files (e.g., in IDE)
+                    var dir = new java.io.File(url.toURI());
+                    for (var file : dir.listFiles()) {
+                        if (file.getName().endsWith(".class")) {
+                            String className = PACKAGE_NAME + "." + file.getName().replace(".class", "");
+                            classes.add(Class.forName(className));
+                        }
+                    }
+                } else if ("jar".equals(protocol)) {
+                    // Running from inside a JAR
+                    String jarPath = url.getPath().substring(5, url.getPath().indexOf("!")); // strip "file:" and "!..."
+                    try (java.util.jar.JarFile jar = new java.util.jar.JarFile(jarPath)) {
+                        var entries = jar.entries();
+                        while (entries.hasMoreElements()) {
+                            var entry = entries.nextElement();
+                            String name = entry.getName();
+                            if (name.startsWith(path) && name.endsWith(".class") && !entry.isDirectory()) {
+                                String className = name.replace('/', '.').replace(".class", "");
+                                classes.add(Class.forName(className));
+                            }
+                        }
+                    }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+    
         return classes;
-    }
+    }    
 }
 
