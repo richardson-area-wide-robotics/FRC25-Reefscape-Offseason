b73d4698f56cea55d163543e9ca0c56555ad04ef
strubium <113206902+strubium@users.noreply.github.com>
2025-08-21 12:56:34 -0500

    just hardcore it for now

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import frc.robot.common.components.RobotExceptionHandler;
 import frc.robot.common.components.RobotUtils;
 import java.nio.file.Path;
 
+import frc.robot.practicum.PracticumInStemContainer;
 import org.lasarobotics.hardware.PurpleManager;
 import org.littletonrobotics.junction.LoggedRobot;
 import com.pathplanner.lib.pathfinding.Pathfinding;
@@ -60,7 +61,7 @@ super();
     Pathfinding.setPathfinder(new LocalADStarAK());
 
     RobotUtils.loadRobotConfig();
-    robotContainer = RobotContainerRegistry.createContainerForTeam(RobotUtils.getTeamNumber());
+    robotContainer = PracticumInStemContainer.createContainer();
 }
 
 
