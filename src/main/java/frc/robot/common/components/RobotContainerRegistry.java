7f16fde578ff23245327af89a5bab442cd5b5f61
strubium <113206902+strubium@users.noreply.github.com>
2025-08-21 23:59:15 -0500

    use Reflections because I AM EVIL

--- src/main/java/frc/robot/common/components/RobotContainerRegistry.java
+++ src/main/java/frc/robot/common/components/RobotContainerRegistry.java
@@ -4,11 +4,11 @@ import frc.robot.practicum.PracticumInStemContainer;
 import frc.robot.common.annotations.Robot;
 import frc.robot.common.interfaces.IRobotContainer;
 import lombok.experimental.UtilityClass;
+import org.reflections.Reflections;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @UtilityClass
 public class RobotContainerRegistry {
@@ -17,76 +17,30 @@ public class RobotContainerRegistry {
     private static final Map<Integer, Class<?>> teamContainers = new HashMap<>();
 
     static {
-        // Register the containers for each team
-        for (Class<?> clazz : getAllClasses()) {
-            if (clazz.isAnnotationPresent(Robot.class)) {
-                Robot annotation = clazz.getAnnotation(Robot.class);
-                teamContainers.put(annotation.team(), clazz);
-            }
+        // Scan for classes annotated with @Robot in the frc.robot package
+        Reflections reflections = new Reflections("frc.robot");
+        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(Robot.class);
+
+        for (Class<?> clazz : annotatedClasses) {
+            Robot annotation = clazz.getAnnotation(Robot.class);
+            teamContainers.put(annotation.team(), clazz);
         }
     }
 
     public static IRobotContainer createContainerForTeam(int teamNumber) {
-        // Try to get the container for the specific team
         Class<?> containerClass = teamContainers.get(teamNumber);
 
-        // If not found, use the default container
         if (containerClass == null) {
-            System.out.println("We cant find a container for " + teamNumber + "! Using default");
-            return PracticumInStemContainer.createContainer(); 
+            System.out.println("We can't find a container for " + teamNumber + "! Using default");
+            return PracticumInStemContainer.createContainer();
         }
 
-        try { //Create the container
+        try {
             return (IRobotContainer) containerClass.getMethod("createContainer").invoke(null);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        return null; 
+        return null;
     }
-
-    private static final String PACKAGE_NAME = "frc.robot";  
-
-    private static List<Class<?>> getAllClasses() {
-        List<Class<?>> classes = new ArrayList<>();
-        String path = PACKAGE_NAME.replace('.', '/');
-    
-        try {
-            var resources = RobotContainerRegistry.class.getClassLoader().getResources(path);
-            while (resources.hasMoreElements()) {
-                var url = resources.nextElement();
-                var protocol = url.getProtocol();
-    
-                if ("file".equals(protocol)) {
-                    // Running from exploded class files (e.g., in IDE)
-                    var dir = new java.io.File(url.toURI());
-                    for (var file : dir.listFiles()) {
-                        if (file.getName().endsWith(".class")) {
-                            String className = PACKAGE_NAME + "." + file.getName().replace(".class", "");
-                            classes.add(Class.forName(className));
-                        }
-                    }
-                } else if ("jar".equals(protocol)) {
-                    // Running from inside a JAR
-                    String jarPath = url.getPath().substring(5, url.getPath().indexOf("!")); // strip "file:" and "!..."
-                    try (java.util.jar.JarFile jar = new java.util.jar.JarFile(jarPath)) {
-                        var entries = jar.entries();
-                        while (entries.hasMoreElements()) {
-                            var entry = entries.nextElement();
-                            String name = entry.getName();
-                            if (name.startsWith(path) && name.endsWith(".class") && !entry.isDirectory()) {
-                                String className = name.replace('/', '.').replace(".class", "");
-                                classes.add(Class.forName(className));
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    
-        return classes;
-    }    
 }
-
